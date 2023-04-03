8831ea1d602fe47fac6d33987a054ab265828031
Santiago <sadavid@unicesar.edu.co>
2023-04-03 13:07:10 -0500

    create new routes and configurate to use in index

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,23 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const admin = require("firebase-admin");
+const cors = require("cors");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+const serviceAccount = require("./admin-credentials.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://restapi-js.firebaseio.com",
+});
+
+app.use(cors({origin: true}));
+
+app.get("/hello-world", (req, res) => {
+  return res.status(200).json({message: "Hello World!"});
+});
+
+// ROUTES
+app.use(require("./routes/products.routes"));
+
+exports.app = functions.https.onRequest(app);
